931c6c34a99d9229e4d45cfa81ba5c9d96719619
Han Paing Htoo <hanpainghtoo1@gmail.com>
2024-01-08 15:46:25 +0630

    import excel data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as XLSX from 'xlsx'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'ng_excel';
+  ExcelData: any;
+  readExcel(event: any){
+    let file = event.target.files[0];
+
+    let fileReader = new FileReader();
+    fileReader.readAsBinaryString(file);
+
+    fileReader.onload = (e)=>{
+      var workBook = XLSX.read(fileReader.result, {type: 'binary'});
+      var sheetNames = workBook.SheetNames;
+      this.ExcelData = XLSX.utils.sheet_to_json(workBook.Sheets[sheetNames[0]]);
+      console.log("ExcelData", this.ExcelData);
+      
+    }
+  }
 }
